b016c2e348bfe73303a39071a94bcc5897e7fdf4
MobileDev0702 <mobiledev0702@hotmail.com>
2021-04-13 10:47:17 +0800

    update app

--- app/src/main/java/com/tokencast/divine/ScanQRActivity.java
+++ app/src/main/java/com/tokencast/divine/ScanQRActivity.java
@@ -1,14 +1,75 @@
 package com.tokencast.divine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class ScanQRActivity extends AppCompatActivity {
+
+    private Bitmap bitmap;
+    private QRGEncoder qrgEncoder;
+    private ImageView iv_qrimage;
+    private String qrCodeStr = "intact_verrier_muds_spot";
+
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanqr);
+
+        constraintLayout = (ConstraintLayout) findViewById(R.id.cl_bg);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(2000);
+
+        iv_qrimage = findViewById(R.id.iv_qrimage);
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+        int smallerDimension = width < height ? width : height;
+        smallerDimension = smallerDimension * 3 / 4;
+
+        qrgEncoder = new QRGEncoder(
+                qrCodeStr, null,
+                QRGContents.Type.TEXT,
+                smallerDimension);
+
+        try {
+            bitmap = qrgEncoder.getBitmap();
+            iv_qrimage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            animationDrawable.start();
+        }
     }
 }
